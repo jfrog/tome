f061a6e9cb8320e33c47b7b99dc1a942cd1a9cd6
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-06-03 12:40:34 +0200

    Copy only first command in console outputs

--- docs/js/tome.js
+++ docs/js/tome.js
@@ -6,7 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
               .replace(/>/g, "&gt;");
   }
 
-  document.querySelectorAll("div.language-console.highlight pre code").forEach(codeElem => {
+  document.querySelectorAll("div.language-console.highlight pre").forEach(preElem => {
+    const copyButton = preElem.querySelector("button.md-clipboard");
+    if (copyButton) {
+      // Remove the default clipboard target, as we will set the text manually
+      delete copyButton.dataset.clipboardTarget;
+    }
+    const codeElem = preElem.querySelector("code")
+    let copyDataHasBeenSet = false;
     const lines = codeElem.textContent.split("\n");
     const html = lines.map(line => {
       if (line.startsWith("# ")) {
@@ -14,7 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
       } else if (line.startsWith("$ ")) {
         const prefix = escapeHTML(line.charAt(0));
         const rest   = escapeHTML(line.slice(1));
-        return `<span class="console-line command-line"><span class="prompt">${prefix}</span><span class="cmd-text">${rest}</span></span>`;
+        if (!copyDataHasBeenSet) {
+          preElem.dataset.clipboardText = rest.trim();
+          // For when the code contains multiple commands, only copy the first one
+          copyDataHasBeenSet = true;
+        }
+        return `<span class="console-line command-line"><span class="prompt">${prefix}</span><span class="cmd-text" id="#${preElem.id}__cmd">${rest}</span></span>`;
       } else if (line === "") {
         return `<span class="console-line empty-line"></span>`;
       } else {
